e2d778c78a8d347eed509b96ec12db0a392022e3
half-slice <junbin2438@naver.com>
2021-05-15 07:12:35 +0000

    maraton

--- programers.js
+++ programers.js
@@ -1,38 +1,16 @@
-function solution(board, moves) { 	//크레인
-    var answer = 0;
-    let bucket=[];
-    let i;
-    let j=0;
-    let z=0;
-    
-    while(z<moves.length){
-        j=moves[z]-1;
-        i=0;
-        if(board[i][j]!=0){
-            bucket.push(board[i][j]);
-			board[i][j]=0;
-        }
+//완주하지 못한 선수
+function solution(participant, completion){
+	var answer = '';
+	participant.sort();
+	completion.sort();
+	
+    for(let i=0; i<participant.length; i++){
         
-        else if(board[i][j]==0){
-            for(i; i<board.length; i++){    
-                if(board[i][j]!=0){
-                    bucket.push(board[i][j]);
-					board[i][j]=0;
-                    break;
-                }
-            }
-        }
-        console.log(bucket, z);
-
-		if(bucket.length>1){
-    		if(bucket[bucket.length-2]==bucket[bucket.length-1]){
-        		bucket.splice(bucket.length-2,2);
-           		answer = answer+2;
-        	}
-    	}
-
-        z++;
+		if(participant[i]!=completion[i]){
+			answer=participant[i];
+			break;
+		}
+		
     }
-    
     return answer;
 }
